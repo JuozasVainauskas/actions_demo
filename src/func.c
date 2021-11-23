de830b6bb67d09b5707977146f20c321f7710903
JuozasVainauskas <vainauskasjuozas@gmail.com>
2021-11-23 22:49:01 +0200

    Add tests, change file structure

---
+++ src/func.c
@@ -0,0 +1,6 @@
+#include "./headers/func.h"
+
+int sum(int first, int second) {
+    return first + second;
+}
+
---
+++ tests/test.c
@@ -0,0 +1,10 @@
+#include "../src/headers/func.h"
+
+int main() {
+    int result = sum(1, 2);
+    if(result == 3) {
+        return 0;
+    }
+    
+    return 1;
+}
